e815cc4b130d3abca5b7f760770333f928019cb1
muhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2023-03-17 05:07:20 +0200

    create articles and gallery section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+let otherLinks = document.querySelector("nav .nav-links li:nth-child(4)");
 let megaMenu = document.querySelector(".mega-menu");
-// megaMenu.onclick = function () {
-//   megaMenu.style.opacity = 1;
-//   megaMenu.style.zIndex = 2;
-// }
+otherLinks.onclick = function () {
+  megaMenu.style.opacity = 1;
+  megaMenu.style.zIndex = 2;
+  megaMenu.style.top = "calc(100% + 1px)";
+  setTimeout(() => {
+    megaMenu.style.opacity = 0;
+    megaMenu.style.zIndex = -1;
+  }, 5000);
+}
